d751230e5f25313ab771480056dfc6b99ef53381
HARSHITA853 <harshita2304.sax@gmail.com>
2025-01-06 00:01:36 +0530

    program 3

---
+++ PowerOf2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdbool.h>
+#include<conio.h>
+bool power(int n){
+    return (n > 0 && (n & (n - 1)) == 0);
+}
+int main(){
+    int n;
+    printf("enter the number to be searched: ");
+    scanf("%d",&n);
+    bool result=power(n);
+    if(result==true){
+        printf("%d is the power of two",n);
+    }
+    else{
+        printf("%d is not the power of two",n);
+    }
+    
+}
